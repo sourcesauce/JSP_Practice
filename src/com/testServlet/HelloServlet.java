ca3ddd3a3c83f004cf4744a27d50bdf72dadb865
SO <SO@DESKTOP-GDR4G99>
2018-11-11 21:35:54 +0900

    Servlet Life-Cycle Test

--- src/com/testServlet/HelloServlet.java
+++ src/com/testServlet/HelloServlet.java
@@ -3,6 +3,8 @@ package com.testServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,10 +29,13 @@ public class HelloServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    
+    // servlet 실행
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		
+		System.out.println("--doGet()--");
 		PrintWriter out = response.getWriter();
 		out.print("<html>");
 		out.print("<head>");
@@ -47,6 +52,27 @@ public class HelloServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+	}
+	
+	@PostConstruct
+	protected void postConstruct() {
+		System.out.println("--postConstruct()--");
+	}
+	
+	// servlet 생성
+	@Override
+	public void init() throws ServletException{
+		System.out.println("--init()--");
+	}
+	// servlet 종료
+	@Override
+	public void destroy() {
+		System.out.println("--destroy()--");
+	}
+	
+	@PreDestroy
+	public void preDestory() {
+			System.out.println("--preDestroy()--");
 	}
 
 }
